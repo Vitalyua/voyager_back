9b0f74a65b002bca8ab585913189381ced057ca2
yura <yura@notan.ua>
2026-04-25 22:51:05 +0800

    fix: change ScanController to a non-final class for extensibility

--- src/Controller/Api/ScanController.php
+++ src/Controller/Api/ScanController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ScanController extends AbstractController
+class ScanController extends AbstractController
 {
     private const string AWB_REGEX = '[0-9]{3}-[0-9]{8}';
 
